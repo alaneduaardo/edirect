aba1a385179bfc2b221e0a68f0ddf76a192cab6a
Alan Eduardo <alan_eduardo@outlook.com.br>
2019-11-10 16:34:30 +0000

    Creating the REST API express structure

---
+++ api/app.js
@@ -0,0 +1,40 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var projectsRouter = require('./routes/projects');
+var todosRouter = require('./routes/todos');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/projects', projectsRouter);
+app.use('/projects/:projectId/todos', todosRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
